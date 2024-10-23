6defe17c043fcb4200f2a8819251f50a29111262
VoxXCode <devanoaghanzaputrapradessah@gmail.com>
2024-10-23 13:49:39 +0700

    Change

---
+++ Mata Kuliah - Pemrograman Dasar/Looping/Looping_1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main () {
+    int a = 1;
+    int b = 0;
+    
+    for (int i = 0; i < 6; i++) {
+        b += 1; // + 1
+        printf("%d \t", a);
+        a += b; // Output a dari hasil sebelumnya + variable b
+        
+        
+         
+        
+        
+    }
+
+    
+    return 0;
+}
---
+++ Mata Kuliah - Pemrograman Dasar/Looping/Looping_2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main () {
+
+    for (int i = 1; i <= 7; i++) {
+        printf("%d \t", i * i);
+    }
+}
