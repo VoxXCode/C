41ba8d34e74e8b94246ecbee203292e1c2e3be72
VoxXCode <devanoaghanzaputrapradessah@gmail.com>
2024-10-25 20:21:31 +0700

    Change

--- List Task Praktikum/Modul 3/CodeLab_1.c
+++ List Task Praktikum/Modul 3/CodeLab_1.c
@@ -7,17 +7,17 @@ int main () {
     scanf("%d", &umur);
 
     if (umur >= 0 && umur <= 120) {
-        if (umur < 2) {
+        if (umur <= 2) { // ======================= Kurang Operator =
             printf("Penumpang mendapatkan tiket gratis. \n");
         } else if (umur >= 2 && umur <= 12) {
             printf("Harga tiket untuk penumpang anak-anak adalah 50%% dari harga normal. \n");
-        } else if (umur >= 12 && umur <= 60) {
+        } else if (umur >= 12 && umur <= 60) { // ====================== Kurang operator =
             printf("Harga tiket untuk penumpang dewasa adalah harga normal. \n");
         } else {
             printf("Harga tiket untuk penumpang lanjut usia adalah 75%% dari harga normal. \n");
         }
     } else {
-            printf("Input tidak valid. Umur harus berada dalam rentang 0 - 120. \n");
+            printf("Input tidak valid. Umur harus berada dalam rentang 0 hingga 120. \n");
     }
 
     return 0;
