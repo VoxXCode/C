86d22f67dc7480adfb57c271600fab2b6a440a5a
VoxXCode <devanoaghanzaputrapradessah@gmail.com>
2024-11-17 21:26:43 +0700

    Change

---
+++ List Task Praktikum/Modul 4/Tugas_Kegiatan_1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int input, total, lanjut;
+
+    do { 
+        total = 0; // Inisialisasi ulang total setiap kali memulai siklus transaksi baru
+        printf("=== Pencatatan Transaksi === \n");
+        printf("Masukkan transaksi selanjutnya (gunakan 0 untuk menyelesaikan):\n");
+
+        do {
+            scanf("%d", &input); // Membaca input transaksi dari pengguna
+            total += input; // Menambahkan input ke total
+        } while (input != 0); // Keluar dari loop jika transaksi bernilai 0
+
+        printf("\nTotal dari transaksi tersebut adalah: %d \n", total);
+
+        printf("Apakah Anda ingin mencatat transaksi lagi? (1 = Ya, 0 = Tidak): ");
+        scanf("%d", &lanjut); // Membaca keputusan pengguna untuk melanjutkan atau berhenti
+        printf("\n");
+    } while (lanjut == 1); // Ulangi proses selama pengguna memilih untuk melanjutkan
+
+    printf("Terima kasih! \n");
+
+    return 0;
+}
